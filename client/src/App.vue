287ab71bbce900ae8af9081f620686a0adf8ca1c
joker <58110797+j0ker404@users.noreply.github.com>
2020-04-25 01:27:39 +0200

    add header component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Heading title="To Do"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import Heading from './components/Header'
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    // HelloWorld
+      Heading
   }
 };
 </script>
 
 <style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ client/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="header">
+        <h1>
+            {{title}}
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Heading',
+    props: {
+        title: String,
+    }
+}
+</script>
+
+<style>
+
+</style>
