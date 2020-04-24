613e011ed03f9ad57aa5fc825ed6624f2d1af381
joker <58110797+j0ker404@users.noreply.github.com>
2020-04-25 01:42:26 +0200

    add listitem and listContent base code

---
+++ client/src/components/ListContent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="lsContent">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ListContent'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/ListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="item">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ListItem'
+}
+</script>
+
+<style scoped>
+
+</style>
